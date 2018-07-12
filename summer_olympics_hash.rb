f393e15156aad15d620c356bc57c5ad7ffb40f13
Emma Davis <emmadavis01@gmail.com>
2018-07-12 17:24:34 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -15,27 +15,16 @@ new_place="Atlanta"
  summer_olympics[new_place]=new_year
  puts summer_olympics
  end
+ 
  puts add_a_key_value_pair
  summer_olympics={:Sydney => "2000", :Athens => "2004", :Beijing => "2008", :London => "2012",:Atlanta => "1996"}
 summer_olympics.each do |places, years|
   puts "The #{years} summer olympics took place in #{places}."
 end
-# def iterate_through_hash
-#   summer_olympics={:Sydney => "2000", :Athens => "2004", :Beijing => "2008", :London => "2012", :Atlanta => "1996"}
-#   places = ["Sydney", "Athens", "Beijing", "London", "Atlanta"]
-# years = ["2000", "2004", "2008", "2012", "1996"]
-# summer_olympics.each do |places, years|
-#   puts "The #{years} summer olympics took place in #{places}."
-#   puts summer_olympics
-# end 
-# end
-# puts iterate_through_hash
-
 
 def iterate_through_keys
  summer_olympics={:Sydney => "2000", :Athens => "2004", :Beijing => "2008", :London => "2012",:Atlanta => "1996"}
  summer_olympics.each do |places, years|
     puts "The #{years} summer olympics took place in #{places.upcase}."
 end
-puts iterate_through_keys
-end
+
