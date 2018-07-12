4b8d9756004b436218dbaadeeb5de3764bbd7a51
Emma Davis <emmadavis01@gmail.com>
2018-07-12 17:18:54 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -34,5 +34,5 @@ end
 
 def iterate_through_keys
  summer_olympics={:Sydney => "2000", :Athens => "2004", :Beijing => "2008", :London => "2012",:Atlanta => "1996"}
- summer_olympics
+ summer_olympics.each_key do |places|
 end
